76d12ea7ad989e7218283d8ed4cec15fb04a8c46
Philip Harrison A <harrisonharry01@gmail.com>
2024-03-07 19:30:27 +0530

    [ FEAT ] - added core utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from .db_utils import DBUtils
 from .cli import CLI
 from .common_utils import CommonUtils
 from .interface_utils import InterfaceUtils
+from .core_utils import CoreUtils
---
+++ utils/core_utils.py
@@ -0,0 +1,65 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from sqlalchemy.orm import Session
+
+from models import Account, Email
+
+from .cli import CLI
+from .common_utils import CommonUtils
+from .db_utils import DBUtils
+
+
+class CoreUtils:
+    @staticmethod
+    def get_new_email_ids(email_ids):
+        new_email_ids = []
+        engine = DBUtils.get_engine()
+
+        
+        with Session(engine) as session:
+            for email_id in email_ids:
+                email = session.query(Email).filter_by(message_id=email_id).first()
+                
+                if not email:
+                    new_email_ids.append(email_id)
+                    
+        return new_email_ids
+
+    @staticmethod
+    def save_email_details(email_address, email_details):
+        engine = DBUtils.get_engine()
+
+        with Session(engine) as session:
+            account = session.query(Account).filter_by(email_id=email_address).first()
+            
+            if not account:
+                account = Account(email_id=email_address)
+                session.add(account)
+                session.commit()
+            
+            email_details["account_id"] = account.id
+            email_details["date"] = CommonUtils.convert_date_string_to_object(email_details["date"])
+            
+            email = Email(**email_details)
+            session.add(email)
+            session.commit()
+        
+    @staticmethod
+    def fetch_and_save_emails(email_manager):
+        email_address = email_manager.get_email_address()
+        email_client = email_manager.get_email_client()
+        
+        email_ids = email_client.fetch_emails(email_address)
+        new_email_ids = CoreUtils.get_new_email_ids(email_ids)
+
+        CLI.display(f"Fetched {len(new_email_ids)} new emails")
+
+        with ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(
+                    email_client.fetch_email_details, email_address, email_id
+                ) for email_id in new_email_ids
+            ]
+
+            for future in as_completed(futures):
+                email_details = future.result()
+                CoreUtils.save_email_details(email_address, email_details)
