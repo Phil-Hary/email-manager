48043280a23dbfb2755cac85afed2e0e685575bc
Philip Harrison A <harrisonharry01@gmail.com>
2024-03-07 19:29:11 +0530

    [ REFACTOR ] - email manager

--- email_manager.py
+++ email_manager.py
@@ -1,68 +1,55 @@
-from utils import CLI, InterfaceUtils
-from gmail_client import GmailClient
-
 from enums import InterfaceEnum
-
+from exceptions import AppError
+from utils import CLI, InterfaceUtils, CoreUtils
 
 class EmailManager:
     def __init__(self):
-        self.__is_authorized = False
         self._is_authorized = False
         self._current_interface = InterfaceEnum.PRE_AUTH
         
-        self.email_client = None
+        self._email_client = None
+        self._email_address = None
     
     def set_current_interface(self, current_interface):
         self._current_interface = current_interface
     
     def set_is_authorized(self, is_authorised):
         self._is_authorized = is_authorised
-
-    def pre_authorization_command_executor(self, command):
-        if command == "1":
-            self.email_client = GmailClient()
-            self.email_client.authorize()
-            self.__is_authorized = True
     
-    def post_authorization_command_executor(self, command):
-        if command == "1":
-            self.email_client.fetch_emails()
-        elif command == "2":
-            self.__actions_activated = True 
-            CLI.display("Choose an action to proceed")
-            CLI.display_menu(["Mark as read", "Move"])
-
-    def execute_command(self, command):
-        if not self.__is_authorized:
-            self.pre_authorization_command_executor(command)
-        else:
-            self.post_authorization_command_executor(command)
+    def set_email_address(self, email_address):
+        self._email_address = email_address
     
-    def display_pre_authorization_menu(self):
-        CLI.display("Choose an email service provider to continue")
-        CLI.display_menu(["Gmail"])
+    def get_email_address(self):
+        if not self._email_address:
+            self._email_address = self._email_client.get_email_address()
+        return self._email_address
+
+    def get_email_client(self):
+        if not self._email_client:
+            raise AppError("Email client is not yet initialized")
+        
+        return self._email_client
+
+    def set_email_client(self, email_client):
+        self._email_client = email_client
     
-    def display_post_authorization_menu(self):
-        CLI.display("Choose an operation to continue")
-        CLI.display_menu(["Fetch emails", "Actions"])
-
-    def display_main_menu(self):
-        if not self.__is_authorized:
-            self.display_pre_authorization_menu()
-        else:
-            self.display_post_authorization_menu()
-
+    def fetch_emails(self):
+        CoreUtils.fetch_and_save_emails(self)
+    
     def driver(self):
         while True:
-            interface = InterfaceUtils.get_interface(self._current_interface)
-            interface.display_menu()
+            try:
+                interface = InterfaceUtils.get_interface(self._current_interface)
+                interface.display_menu()
 
-            choice = input()
+                choice = input()
 
-            if choice == "0":
-                break
-                
-            interface.command_handler(self, choice)
+                if choice == "0":
+                    break
+                    
+                interface.command_handler(self, choice)
+            except AppError as e:
+                CLI.display(e.message)
 
 
 if __name__ == "__main__": 
