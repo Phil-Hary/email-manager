fa6ebd86142ac8be6d2a71d5352bf2e49e11afea
Philip Harrison A <harrisonharry01@gmail.com>
2024-03-07 19:32:24 +0530

    [ FEAT ] - updated date formatting

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -3,6 +3,6 @@ from datetime import datetime
 class CommonUtils:
 
     @staticmethod
-    def convert_date_string_to_object(date_string, date_format="%a, %d %b %Y %H:%M:%S %z (%Z)"):
+    def convert_date_string_to_object(date_string, date_format="%a, %d %b %Y %H:%M:%S %z"):
         date_time_obj = datetime.strptime(date_string, date_format)
         return date_time_obj
