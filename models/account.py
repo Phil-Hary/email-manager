db0ecd85737b382e38345b2333c69016c1740cac
Philip Harrison A <harrisonharry01@gmail.com>
2024-03-08 08:25:39 +0530

    [ REFACTOR ] - account

--- models/account.py
+++ models/account.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
 
