592c9b0eb9c37e6320159ca69cbb67fec86cb5bd
Philip Harrison A <harrisonharry01@gmail.com>
2024-03-08 08:26:22 +0530

    [ FEAT ] - handled exceptions

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 
+from exceptions import AppError
+
 class CommonUtils:
 
     @staticmethod
     def convert_date_string_to_object(date_string, date_format="%a, %d %b %Y %H:%M:%S %z"):
-        date_time_obj = datetime.strptime(date_string, date_format)
+        try:
+            date_time_obj = datetime.strptime(date_string, date_format)
+        except Exception:
+            raise AppError("An error occurred during date conversion")
+        
         return date_time_obj
