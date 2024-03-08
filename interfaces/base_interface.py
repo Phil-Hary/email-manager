42ea8a3817c95a418dcf5add551387f6feb5eba4
Philip Harrison A <harrisonharry01@gmail.com>
2024-03-08 10:11:51 +0530

    [ CHORE ] - comments

--- interfaces/base_interface.py
+++ interfaces/base_interface.py
@@ -1,10 +1,20 @@
 from abc import ABC, abstractmethod
 
 class BaseInterface(ABC):
+    """
+        This is the base abstract class for all the interfaces. Any user interface must
+        inherit this and override the abstract methods
+    """
     @abstractmethod
     def display_menu():
+        """
+            Description: This method must be overridden and defined with the logic to display the menu for a particular screen
+        """
         pass
 
     @abstractmethod
     def command_handler():
+        """
+            Description: This method must be overridden and defined with the logic to handle the menu options
+        """
         pass
